7bef3a64b0860df964b13d4ff3087a7d0181daaa
Zain Shaikh <szain2024@gmail.com>
2026-07-19 23:08:05 +0530

    fix(cors): configure frontend origin

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+import { env } from "./config/env";
 import authRoutes from "./modules/auth/auth.routes"
 import inviteRoutes from "./modules/invite/invite.routes";
 import { errorHandler } from "./middlewares/errorHandler";
@@ -9,7 +10,12 @@ import { requestLogger } from "./middlewares/requestLogger";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(requestLogger);
 app.use(cookieParser());
